ae4309bc185efb739f5da5ff130f1502fb9243d8
notryanb <notryanb@gmail.com>
2016-11-01 23:07:45 -0400

    Rewrite for handling one request

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
+use std::io::{Read, Write, BufReader, BufRead};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:2345").unwrap();
+    let stream = listener.accept().unwrap().0;
+    handle_request(stream);
+}
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    println!("Success!");
-                });
-            }
-            Err(e) => { println!("Failed");
-            }
+fn handle_request(stream: TcpStream) {
+
+    let mut reader = BufReader::new(stream);
+
+    for line in reader.by_ref().lines() {
+        if line.unwrap() == "" {
+            break;
         }
     }
-    drop(listener);
+
+    send_response(reader.into_inner());
 }
+
+fn send_response(mut stream: TcpStream) {
+    // Write the header and the html body
+    let response = "HTTP/1.1 200 OK\n\n<html><body><h1>Hello, World!</h1></body></html>";
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
